c971ef8ab0567e751146c8e1faa3bfff5b35ced8
Shivam Sharma <sdsharma@eng.ucsd.edu>
2017-07-23 17:13:33 -0700

    Initial Push

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    styleUrls: ['./home.component.scss'],
+    templateUrl: './home.component.html',
+})
+export class HomeComponent {
+
+}
